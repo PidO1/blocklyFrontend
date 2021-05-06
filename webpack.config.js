0d816da3a7069169f04701d279af0c070ab3a6fc
Pieter <23745584@g.nwu.ac.za>
2021-05-06 16:43:27 +0200

    try

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+ "mode": "none",
+ "entry": "./src/index.js",
+ "output": {
+   "path": __dirname + '/build',
+   "filename": "bundle.js"
+ },
+devServer: {
+   contentBase: path.join(__dirname, 'build')
+ } , 
+ "module": {
+    "rules": [
+      {
+        "test": /\.css$/,
+        "use": [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+    ]
+  }
+ 
+}
