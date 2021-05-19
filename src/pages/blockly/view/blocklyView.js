3c62c1d10d25bd36ab27f606f9f39b6625164d31
Armin van Wyk <u18008632@tuks.co.za>
2021-05-19 18:27:28 +0200

    fixed workspace and blocks display issues on section tag

--- src/pages/blockly/view/blocklyView.js
+++ src/pages/blockly/view/blocklyView.js
@@ -13,7 +13,7 @@ export default class extends AbstractView {
     }
 
     afterInit() {
-        // var blocklyArea = document.getElementById('blocklyArea');
+        var blocklyArea = document.getElementById('blocklyArea');
         var blocklyDiv = document.getElementById('blocklyDiv');
         var demoWorkspace = Blockly.inject(blocklyDiv,
             {
@@ -51,27 +51,61 @@ export default class extends AbstractView {
     //@html:start
 async getHtml() {
 return `
-<section class="blockly-workspace">
-    <div class="middle">
-        <!-- <table>
-            <tr>
-                <td id="blocklyArea" />
-            </tr>
-        </table> -->
-        <div id="blocklyDiv" />
-        <xml id="toolbox" style="display: none">
-            <block type="controls_if" />
-            <block type="logic_compare" />
-            <block type="controls_repeat_ext" />
-            <block type="math_number">
-                <field name="NUM">123</field>
-            </block>
-            <block type="math_arithmetic" />
-            <block type="text" />
-            <block type="text_print" />
-        </xml>
-    </div>
-</section>
+<table style="height:90vh;width:100%;background-color:#fff;font-family:sans-serif;overflow:hidden;">
+    <tr>
+        <td id="blocklyArea">
+        </td>
+    </tr>
+</table>
+<div id="blocklyDiv" style="position: absolute;width:781px;height:591px;"></div>
+<div id="textarea"></div>
+
+<xml  id="toolbox" style="display: none">
+    <block type="controls_if"></block>
+    <block type="logic_compare"></block>
+   <block type="controls_repeat_ext"></block>
+    <block type="math_number">
+        <field name="NUM">123</field>
+    </block>
+    <block type="math_arithmetic"></block>
+    <block type="text"></block>
+    <block type="text_print"></block>
+</xml>
+<script>
+    var blocklyArea = document.getElementById('blocklyArea');
+    var blocklyDiv = document.getElementById('blocklyDiv');
+    var demoWorkspace = Blockly.inject(blocklyDiv,
+        {media: '../../media/',
+            toolbox: document.getElementById('toolbox')});
+    var onresize = function(e) {
+        // Compute the absolute coordinates and dimensions of blocklyArea.
+        var element = blocklyArea;
+        var x = 0;
+        var y = 0;
+        do {
+            x += element.offsetLeft;
+            y += element.offsetTop;
+            element = element.offsetParent;
+        } while (element);
+        // Position blocklyDiv over blocklyArea.
+        blocklyDiv.style.left = x + 'px';
+        blocklyDiv.style.top = y + 'px';
+        //blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+        //blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+        Blockly.svgResize(demoWorkspace);
+    };
+    window.addEventListener('resize', onresize, false);
+    onresize();
+    Blockly.svgResize(demoWorkspace);
+    function myUpdateFunction() {
+        var xml = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+        document.getElementById('blocklyDiv').setAttribute("class", "flipper");
+        document.getElementById('textarea').setAttribute("class", "flipback");
+        document.getElementById('textarea').innerText = xml;
+    }
+
+</script>
+
 `;
 }
 //@html:end
